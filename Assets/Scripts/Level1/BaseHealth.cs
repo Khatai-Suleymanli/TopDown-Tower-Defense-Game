ac2b0a191573906fb65ada28da10e7586bc6c00d
Khatai Suleymanli <khatai.suleymanli05@gmail.com>
2025-02-27 16:12:32 +0100

    UI

---
+++ Assets/Scripts/Level1/BaseHealth.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class BaseHealth : MonoBehaviour
+{
+    public int Health = 100;
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        Debug.Log("Base took damage!");
+
+        if (Health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+        Debug.Log(Health);
+    }
+}
---
+++ Assets/Scripts/Level1/BaseShooting.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseShooting : MonoBehaviour
+{
+    public float AttackRange = 10f;
+    public float FireRate = 1f;
+    public int damage = 30;
+    private float NextFireTime;
+
+
+    public Transform shootPoint;
+
+    [Header("------MUZZLE-------")]
+    public ParticleSystem muzzleEffect;
+
+    //for ilustrating range
+    public float radious = 5f;
+    public Color color = Color.green; // color of the range circle of the tower cannons
+
+
+    [Header("------Visualizing-------")]
+    public Color lineColor = Color.red;
+    public float thickness = 0.05f;
+
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        
+        Gizmos.DrawWireSphere( transform.position, AttackRange );
+
+
+
+        ///////
+        Gizmos.color = lineColor;
+
+        Vector3 start =  shootPoint.transform.position;
+        Vector3 end = shootPoint.transform.position + shootPoint.transform.forward;
+
+
+        Gizmos.DrawLine(start, end);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+
+        
+       
+
+        foreach (GameObject enemy in enemies) {
+
+            Vector3 forwardTurret = shootPoint.transform.forward; // our turret's forward vector
+
+
+            Vector3 toEnemyVector =enemy.transform.position - transform.position;  // bizden dusmene vector
+
+
+            float dotValue = Vector3.Dot(forwardTurret, toEnemyVector);
+
+            if (Vector3.Distance(transform.position, enemy.transform.position) <= AttackRange && Time.time > NextFireTime && dotValue >=0.98) {
+                //shoot enemy
+                ShootEnemy(enemy);
+                NextFireTime = FireRate + Time.time;
+                break; //shooting one enemy at a time
+            }
+        }
+    }
+
+    void ShootEnemy(GameObject enemy) {
+
+        if (muzzleEffect != null) // error handling
+        {
+            muzzleEffect.Play();
+        }
+
+        // error handling
+        enemy.GetComponent<BaseHealth>().TakeDamage(damage); // replace with enemyHealth script.
+        Debug.Log("Base shot the enmy");
+
+    }
+}
